e4d28ffd4d89ed3e57cd2a0754bd2d3164063335
Talita Sousa <talitamarrri@gmail.com>
2025-08-11 15:41:54 -0300

    feat: adicionando a validação do token JWT

--- src/main/java/cuidar/mais/api/service/TokenService.java
+++ src/main/java/cuidar/mais/api/service/TokenService.java
@@ -4,6 +4,7 @@ package cuidar.mais.api.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import cuidar.mais.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,16 +20,30 @@ public class TokenService {
     private String secret;
 
     public String gerarToken(Usuario usuario) {
+        
         try {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer("API Cuidar.mais")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
-            throw new RuntimeException("erro ao gerrar token jwt", exception);
+            throw new RuntimeException("erro ao gerar token jwt", exception);
         }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+
+                    .withIssuer("API Cuidar.mais")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token jwt inválido ou expirado!");        }
     }
 
     private Instant dataExpiracao() {
